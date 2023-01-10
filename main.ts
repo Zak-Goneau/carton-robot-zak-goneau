26583430b78a04fe58c8f05e836f70a19e5bd6ab
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-09 20:14:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,14 +7,29 @@ radio.onReceivedNumber(function (receivedNumber) {
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
 })
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(300)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(300)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(300)
+    }
+})
 let angle = 0
 let opened = 95
 let closed = 175
 basic.forever(function () {
-    led.plotBarGraph(
-    input.lightLevel(),
-    0
-    )
     pins.servoWritePin(AnalogPin.P0, input.lightLevel())
 })
 basic.forever(function () {
@@ -26,4 +41,9 @@ basic.forever(function () {
     closed
     )
     pins.servoWritePin(AnalogPin.P0, angle)
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.ThreeG)) {
+        music.playMelody("C D G F E D F C ", 268)
+    }
 })
