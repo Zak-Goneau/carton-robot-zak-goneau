205453e3d07f2db1ad5f7eadaac229ed6ed6206e
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-11 10:21:33 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,29 +7,14 @@ radio.onReceivedNumber(function (receivedNumber) {
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
 })
-input.onButtonPressed(Button.B, function () {
-	
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(400)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.pause(300)
-        basic.showIcon(IconNames.Heart)
-        basic.pause(400)
-        basic.showIcon(IconNames.SmallHeart)
-        basic.pause(300)
-    }
-})
 let angle = 0
-pins.setAudioPin(AnalogPin.P1)
 let opened = 95
 let closed = 175
 basic.forever(function () {
-    pins.servoWritePin(AnalogPin.P0, input.lightLevel())
-})
-basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
     angle = pins.map(
     input.lightLevel(),
     0,
@@ -38,4 +23,11 @@ basic.forever(function () {
     closed
     )
     pins.servoWritePin(AnalogPin.P0, angle)
+})
+basic.forever(function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    0
+    )
+    pins.servoWritePin(AnalogPin.P0, input.lightLevel())
 })
