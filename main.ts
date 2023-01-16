99c49928027107b8aa6c166812c08bbebf250ab8
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-16 11:04:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,11 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    pins.servoWritePin(AnalogPin.P0, 0)
-    basic.pause(500)
-    pins.servoWritePin(AnalogPin.P0, 180)
-    basic.pause(500)
-})
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(0)
-})
-let angle = 0
-let opened = 95
-let closed = 175
+let opened = -90
+let closed = -270
 basic.forever(function () {
-    led.plotBarGraph(
-    input.lightLevel(),
-    0
-    )
-    angle = pins.map(
-    input.lightLevel(),
-    0,
-    255,
-    opened,
-    closed
-    )
-    pins.servoWritePin(AnalogPin.P0, angle)
+	
 })
 basic.forever(function () {
-    led.plotBarGraph(
-    input.lightLevel(),
-    0
-    )
-    pins.servoWritePin(AnalogPin.P0, input.lightLevel())
+    pins.servoWritePin(AnalogPin.P0, opened)
+    basic.pause(1000)
+    pins.servoWritePin(AnalogPin.P0, closed)
+    basic.pause(1000)
 })
